eff81a6cb4e57e40df63bf7f24dae1843e8fd33b
Nguyen Quoc <nguyenhuuquoc19@gmail.com>
2022-07-01 00:27:07 +0700

    main

---
+++ asset/js/custom-owl.js
@@ -0,0 +1,17 @@
+// Partner list
+$(".logos .menu-list").owlCarousel({
+    loop: true,
+    margin: 66,
+    nav: true,
+    responsive: {
+        0: {
+            items: 1
+        },
+        768: {
+            items: 3,
+        },
+        1000: {
+            items: 7,
+        },
+    },
+});
