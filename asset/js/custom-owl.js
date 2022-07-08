12951272eafe3e96b3405157fb9c57765f168b1b
NguyenQuoc <nguyenhuuquoc19@gmail.com>
2022-07-08 11:22:04 +0700

    fix

--- asset/js/custom-owl.js
+++ asset/js/custom-owl.js
@@ -1,128 +1,134 @@
-// Partner list
-$(".logos .menu-list").owlCarousel({
-    loop: true,
-    nav: false,
-    dots: false,
-    autoplay:true,
-    autoplayTimeout:5000,
-    autoplayHoverPause:true,
-    responsive: {
-        0: {
-            items: 3,
-            margin: 40,
+
+$(document).ready(function () {
+    $(".logos .menu-list").owlCarousel({
+        loop: true,
+        nav: false,
+        dots: false,
+        autoplay:true,
+        autoplayTimeout:5000,
+        autoplayHoverPause:true,
+        responsive: {
+            0: {
+                items: 3,
+                margin: 40,
+            },
+            540: {
+                items: 3,
+                margin: 40,
+            },
+            768: {
+                items: 5,
+                margin: 50,
+            },
+            1400: {
+                items: 7,
+                margin: 66,
+            }
         },
-        540: {
-            items: 3,
-            margin: 40,
+    });
+    
+    $(".best-sellers .list-product").owlCarousel({
+        loop: true,
+        nav: true,
+        dots: true,
+        dotsEach: 3,
+        autoplay:true,
+        autoplayTimeout:3000,
+        autoplayHoverPause:true,
+        responsive: {
+            0: {
+                items: 2,
+                margin: 20,
+                dotsEach: 2,    
+            },
+            540: {
+                items: 2,
+                margin: 20,
+                dotsEach: 2,
+            },
+            768: {
+                items: 3,
+                margin: 20,
+                dotsEach: 3,
+            },
         },
-        768: {
-            items: 5,
-            margin: 50,
+    });
+    
+    $(".our-fabrics .main-list").owlCarousel({
+        loop: true,
+        nav: true,
+        dots: true,
+        autoplay:true,
+        autoplayTimeout:3000,
+        autoplayHoverPause:true,
+        responsive: {
+            0: {
+                items: 2,
+                margin: 20,
+            },
+            540: {
+                items: 2,
+                margin: 10,
+            },
+            768: {
+                items: 3,
+                margin: 30,
+            },
+            1400: {
+                items: 4,
+                margin: 30,
+            }
         },
-        1400: {
-            items: 7,
-            margin: 66,
-        }
-    },
+    });
+    
+    $(".list-product").owlCarousel({
+        loop: true,
+        nav: true,
+        dots: true,
+        autoplay:true,
+        autoplayTimeout:3000,
+        autoplayHoverPause:true,
+        responsive: {
+            0: {
+                items: 2,
+                margin: 20,
+            },
+            540: {
+                items: 2,
+                margin: 20,
+            },
+            768: {
+                items: 3,
+                margin: 30,
+            },
+            1000: {
+                items: 4,
+                margin: 30,
+            }
+        },
+    });
+    
+    $('.share .main-list').owlCarousel({
+        loop: true,
+        nav: false,
+        dots: false,
+        autoplay:true,
+        autoplayTimeout:5000,
+        autoplayHoverPause:true,
+        responsive: {
+            0: {
+                items: 3,
+                margin: 20,
+            },
+            768: {
+                items: 4,
+                margin: 10,
+            },
+    
+            1000: {
+                items: 5,
+                margin: 15,
+            }
+        },
+    })
 });
-
-$(".best-sellers .list-product").owlCarousel({
-    loop: true,
-    nav: false,
-    dots: false,
-    autoplay:true,
-    autoplayTimeout:3000,
-    autoplayHoverPause:true,
-    responsive: {
-        0: {
-            items: 2,
-            margin: 20,
-        },
-        540: {
-            items: 2,
-            margin: 20,
-        },
-        768: {
-            items: 3,
-            margin: 20,
-        },
-    },
-});
-
-$(".our-fabrics .main-list").owlCarousel({
-    loop: true,
-    nav: false,
-    dots: false,
-    autoplay:true,
-    autoplayTimeout:3000,
-    autoplayHoverPause:true,
-    responsive: {
-        0: {
-            items: 2,
-            margin: 20,
-        },
-        540: {
-            items: 2,
-            margin: 10,
-        },
-        768: {
-            items: 3,
-            margin: 30,
-        },
-        1400: {
-            items: 4,
-            margin: 30,
-        }
-    },
-});
-
-$(".list-product").owlCarousel({
-    loop: true,
-    nav: false,
-    dots: false,
-    autoplay:true,
-    autoplayTimeout:3000,
-    autoplayHoverPause:true,
-    responsive: {
-        0: {
-            items: 2,
-            margin: 20,
-        },
-        540: {
-            items: 2,
-            margin: 20,
-        },
-        768: {
-            items: 3,
-            margin: 30,
-        },
-        1000: {
-            items: 4,
-            margin: 30,
-        }
-    },
-});
-
-$('.share .main-list').owlCarousel({
-    loop: true,
-    nav: false,
-    dots: false,
-    autoplay:true,
-    autoplayTimeout:5000,
-    autoplayHoverPause:true,
-    responsive: {
-        0: {
-            items: 3,
-            margin: 20,
-        },
-        768: {
-            items: 4,
-            margin: 10,
-        },
-
-        1000: {
-            items: 5,
-            margin: 15,
-        }
-    },
-})
--- asset/js/main.js
+++ asset/js/main.js
@@ -2,29 +2,50 @@ $(document).ready(function() {
      // reponsive
      let width = $(window).width();
 
-     if(width <= 768) {
-        $('.header-left .menu').hide()
-        $('.header-right .menu .menu-list li:first a').appendTo($('.icon-hidden'));
-        $('.header-left').after($('.header-logo'));
-        $('.footer-col').addClass('footer-collaps-btn');
-        $('.footer-col:last').removeClass('footer-collaps-btn');
-        $('.guide .main-title').after($('.guide .container img'));
-        $('.about .main-title').after($('.about  .container img'));
-        
-        let collaps = document.getElementsByClassName('footer-collaps-btn');
-        $('.footer-collaps-btn').on('click', function() {
-            if($('.footer-collaps-btn').hasClass('active')) {
-                $(this).removeClass('active');
-            } else {
-                $(this).addClass('active');
-                $(this).children('menu').css("max-height", "20px");
-            }
-        })
-     }
-
-     if(width <= 540) {
-        $('.banner img').attr('src', './asset/image/banner/banner-mb.png');
-     }
-
+     $(window).on('scroll load', function(){
+        if(width <= 768) { 
+            $('.header-left .menu').hide()
+            $('.header-right .menu .menu-list li:first a').appendTo($('.icon-hidden'));
+            $('.header-left').after($('.header-logo'));
+            $('.footer-col').addClass('footer-collaps-btn');
+            $('.footer-col:last').removeClass('footer-collaps-btn');
+            $('.guide .main-title').after($('.guide .container img'));
+            $('.about .main-title').after($('.about  .container img'));
+    
+            let collaps = document.getElementsByClassName('footer-collaps-btn');
+            $('.footer-collaps-btn').on('click', function() {
+                if($('.footer-collaps-btn').hasClass('active')) {
+                    $(this).removeClass('active');
+                } else {
+                    $(this).addClass('active');
+                    $(this).children('menu').css("max-height", "20px");
+                }
+            })
+         }
+    
+         if(width <= 540) {
+            $('.banner img').attr('src', './asset/image/banner/banner-mb.png');
+         }
+     })
+     
+     
      $(window).on('resize', function() {location.reload()});
+
+     $(window).on('load', function() {
+        $('.spinner').addClass('spinner--hidden');
+
+        $('.spinner').on("transitionend", function() {
+            document.body.removeChild('spinner');
+        });
+     })
+     
+     $(window).on('scroll', function() {
+        $(this).scrollTop() ? $('.back-top').fadeIn() : $('.back-top').fadeOut();
+     });
+
+     $('.back-top').click(function () { 
+        $('html, body').animate({
+            scrollTop: 0,
+        }, 300);
+     });
 })
